7844348379663e63fa7815c60baef6d43eba4e76
jason900326 <131471313+jason900326@users.noreply.github.com>
2026-09-08 18:26:25 +0800

    Load four-path home journey styles

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ import "./phase10-conversation-ui.css";
 import "./phase10-tarot-immersion.css";
 import "./phase10-tarot-flow-v2.css";
 import "./phase10-tarot-dialogue.css";
+import "./phase10-home-journey.css";
 
 export const metadata = {
   title: "Vela",
