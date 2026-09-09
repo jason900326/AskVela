0d10d9e0ec416dc5355401600562f0d83286b928
jason900326 <131471313+jason900326@users.noreply.github.com>
2026-09-09 17:41:56 +0800

    Load Phase 12A monetization UI styles

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,7 @@ import "./phase10-final-fixes.css";
 import "./phase10-auth-waiting.css";
 import "./phase10-retention-v1.css";
 import "./phase10-history-exit.css";
+import "./phase12a-monetization-ui.css";
 
 export const metadata = {
   title: "Vela",
