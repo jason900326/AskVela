22d0a9cd8bfc4c4a8c2b9147b911ef5e7c08962d
jason900326 <131471313+jason900326@users.noreply.github.com>
2026-09-09 23:47:14 +0800

    Load Phase 12A Free flow fixes last

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,7 @@ import "./phase10-history-exit.css";
 import "./phase12a-monetization-ui.css";
 import "./phase12a-flip-pages.css";
 import "./phase12a-immersive-tarot.css";
+import "./phase12a-free-flow-fixes.css";
 
 export const metadata = {
   title: "Vela",
